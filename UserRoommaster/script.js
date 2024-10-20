0370379ac79967cd525b250f016682da3f263c27
Thangnguyen <nguyencongthang266@gmail.com>
2024-10-20 15:51:59 +0700

    3th commit

--- UserRoommaster/script.js
+++ UserRoommaster/script.js
@@ -1,13 +1,3 @@
-// Gán sự kiện khi chọn một mục trong dropdown
-const items = document.querySelectorAll(".dropdown-item");
-const roomTypeInput = document.getElementById("roomTypeInput");
-
-items.forEach((item) => {
-  item.addEventListener("click", function () {
-    roomTypeInput.value = this.textContent; // Gán giá trị được chọn vào input
-  });
-});
-
 document.getElementById("check-in").addEventListener("change", function () {
   let checkInDate = new Date(this.value);
   let checkOutInput = document.getElementById("check-out");
@@ -18,6 +8,18 @@ document.getElementById("check-in").addEventListener("change", function () {
 
 // Lấy các phần tử HTML
 document.addEventListener("DOMContentLoaded", function () {
+  const items = document.querySelectorAll(".dropdown-item");
+  console.log(items);
+
+  items.forEach((item) => {
+    item.addEventListener("click", function (e) {
+      e.preventDefault();
+      console.log(item);
+
+      roomTypeInput.value = this.innerHTML; // Gán giá trị được chọn vào input
+    });
+  });
+
   const decreaseBtn = document.getElementById("decrease");
   const increaseBtn = document.getElementById("increase");
   const roomCount = document.getElementById("roomCount");
@@ -32,6 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Sự kiện tăng số phòng
   increaseBtn.addEventListener("click", function () {
+    console.log("hehe");
     let currentValue = parseInt(roomCount.value);
     if (currentValue < 80) {
       // Giới hạn số lượng phòng tối đa là 80
@@ -39,3 +42,37 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+const track = document.querySelector(".bang-chuyen-track");
+let currentPosition = 0;
+const itemWidth = document.querySelector(".bang-chuyen-item").clientWidth;
+const itemsVisible = 3; // Số lượng card có thể nhìn thấy cùng lúc
+const totalItems = track.children.length; // Tổng số lượng card
+const maxPosition = (totalItems - itemsVisible) * (itemWidth + 20); // 20 là khoảng cách margin giữa các card
+
+function moveLeft() {
+  if (currentPosition > 0) {
+    currentPosition -= itemWidth + 20; // Trừ cả phần margin
+    track.style.transform = `translateX(-${currentPosition}px)`;
+  }
+}
+
+function moveRight() {
+  if (currentPosition < maxPosition) {
+    currentPosition += itemWidth + 20; // Cộng cả phần margin
+    track.style.transform = `translateX(-${currentPosition}px)`;
+  }
+}
+
+function DichVu(DvuId) {
+  const TatcaDvu = document.getElementsByClassName("dvu");
+
+  // Ẩn tất cả các div
+  for (let i = 0; i < TatcaDvu.length; i++) {
+    TatcaDvu[i].style.display = "none";
+  }
+
+  // Hiển thị div được chọn
+  const LuachonDvu = document.getElementById(DvuId);
+  LuachonDvu.style.display = "block";
+}
